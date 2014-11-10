121f26f95ca0a52c1e809908b849968631b3774d
Stefan Varbanov <stefanvarbanoff@abv.bg>
2014-11-11 01:08:17 +0200

    web projects created

---
+++ Source/SocialWorker.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SocialWorker.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
