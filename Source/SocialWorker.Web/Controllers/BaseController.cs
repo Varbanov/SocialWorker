94c023f6532eb2f6d0f689a664039ac136b6e998
Stefan Varbanov <stefanvarbanoff@abv.bg>
2014-11-14 20:45:20 +0200

    added role checking in base controller

--- Source/SocialWorker.Web/Controllers/BaseController.cs
+++ Source/SocialWorker.Web/Controllers/BaseController.cs
@@ -1,19 +1,22 @@
 ﻿namespace SocialWorker.Web.Controllers
 {
     using System.Web.Mvc;
+    using System;
+    using System.Linq;
 
     using SocialWorker.Web.ViewModels;
     using System.Data.Entity;
     using SocialWorker.Data.Models;
     using SocialWorker.Data;
     using Microsoft.AspNet.Identity;
-using SocialWorker.Web.Infrastructure.UserProvider;
+    using SocialWorker.Web.Infrastructure.UserProvider;
+    using SocialWorker.Common;
 
     public abstract class BaseController
     {
         private string currentUserId;
 
-        public BaseController(ISocialWorkerData data, IUserProvider userProvider )
+        public BaseController(ISocialWorkerData data, IUserProvider userProvider)
         {
             this.Data = data;
             this.currentUserId = userProvider.GetUserId();
@@ -23,7 +26,33 @@ using SocialWorker.Web.Infrastructure.UserProvider;
 
         protected string CurrentUserId { get; set; }
 
+        protected bool IsInRole(string userId, string roleName)
+        {
+            var user = this.Data.Users.GetById(userId);
 
+            if (user == null)
+            {
+                return false;
+            }
 
+            var roles = ((SocialWorkerDbContext)this.Data.Context)
+                .Roles
+                .Select(r => new { r.Id, r.Name }).ToList();
+
+            foreach (var role in user.Roles)
+            {
+                var currentRole = roles.FirstOrDefault(r => r.Id == role.RoleId);
+                if (currentRole != null)
+                {
+                    if (currentRole.Name == roleName)
+                    {
+                        return true;
+                    }
+                }
+
+            }
+
+            return false;
+        }
     }
 }
